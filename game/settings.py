e8131067e444752c6880d1a9c9d0dc0129633a77
Daddou-iut <amiminada352@gmail.com>
2026-03-07 20:26:16 +0100

    ajout du front au back

--- game/settings.py
+++ game/settings.py
@@ -142,7 +142,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5174",
     "http://localhost:5174",
-    "https://ultimate-pastry.vercel.app/",
+    "https://ultimate-pastry.vercel.app",
 ]
 
 # Permet l'envoi de cookies et credentials
